641534545eee54f1d68a29f356e0dd7eb87422ec
jlmcavalcante <joseluismoreiracavalcante@gmail.com>
2025-02-04 00:52:46 -0300

    melhorar estilo dos nós

--- src/app/mermaid-diagram/components/gojs-flowchart.component.ts
+++ src/app/mermaid-diagram/components/gojs-flowchart.component.ts
@@ -2,6 +2,8 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import * as go from 'gojs';
 import { SerpentineLayout } from './serpentine-layout';
 import { MARCOS_PROCESSO } from '../data/marcos-processo.data';
+import html2canvas from 'html2canvas';
+
 
 @Component({
   selector: 'app-gojs-flowchart',
@@ -11,6 +13,13 @@ import { MARCOS_PROCESSO } from '../data/marcos-processo.data';
 })
 export class GojsFlowchartComponent implements AfterViewInit {
   @ViewChild('diagramDiv', { static: true }) private diagramRef!: ElementRef;
+  @ViewChild('captureDiv') captureDiv!: ElementRef;
+  public dynamicHeight: number = 0;
+
+  constructor() {
+    const flochartLines: number = 3;
+    this.dynamicHeight = 450 * flochartLines;
+  }
 
   ngAfterViewInit() {
     const $ = go.GraphObject.make;
@@ -22,6 +31,8 @@ export class GojsFlowchartComponent implements AfterViewInit {
         spacing: new go.Size(120, 90),
       }),
       isReadOnly: true,
+      allowVerticalScroll: false,
+      allowHorizontalScroll: false,
     });
 
     function definirCores(tipo: string): { border: string; background: string } {
@@ -51,11 +62,11 @@ export class GojsFlowchartComponent implements AfterViewInit {
         new go.Binding('fill', 'tipo_ato', (tipo) => definirCores(tipo).background)
       ),
 
-      $(go.Panel, 'Vertical', { padding: 12, alignment: go.Spot.Center, defaultAlignment: go.Spot.Left },
+      $(go.Panel, 'Vertical', { alignment: go.Spot.Center, defaultAlignment: go.Spot.Left, padding: 10},
 
         $(go.Panel, "Auto",
-          { stretch: go.GraphObject.Fill },
-          $(go.Shape, "RoundedRectangle", { fill: "#FFFFFF", stroke: "transparent", parameter1: 10 }),
+          { stretch: go.Stretch.Fill },
+          $(go.Shape, "RoundedRectangle", { fill: "#FFFFFF", stroke: "transparent", parameter1: 5 }),
           $(go.TextBlock,
             {
               font: 'bold 14px sans-serif',
@@ -63,37 +74,39 @@ export class GojsFlowchartComponent implements AfterViewInit {
               stroke: 'black',
               wrap: go.Wrap.Fit,
               margin: new go.Margin(8, 0, 8, 0),
-              maxSize: new go.Size(280, NaN),
+              maxSize: new go.Size(250, NaN),
             },
             new go.Binding('text', 'title')
           )
         ),
 
-        $(go.Panel, "Horizontal", { defaultAlignment: go.Spot.Left, margin: new go.Margin(20, 0, 0, 0) },
-          $(go.TextBlock, { font: 'bold 12px sans-serif', stroke: '#000', margin: new go.Margin(0, 4, 0, 0), text: "Data:" }),
-          $(go.TextBlock, { font: '12px sans-serif', stroke: '#000', width: 220, wrap: go.TextBlock.WrapFit }, new go.Binding('text', 'data'))
+        $(go.Panel, "Horizontal", { defaultAlignment: go.Spot.Left, margin: new go.Margin(15, 0, 0, 0) },
+          $(go.TextBlock, { font: 'bold 12px sans-serif', stroke: '#000', margin: new go.Margin(0, 8, 0, 10), text: "Data:" }),
+          $(go.TextBlock, { font: '14px sans-serif', stroke: '#000', width: 220, wrap: go.Wrap.Fit }, new go.Binding('text', 'data'))
         ),
 
-        $(go.Panel, "Horizontal", { defaultAlignment: go.Spot.Left, margin: new go.Margin(10, 0, 0, 0) },
-          $(go.TextBlock, { font: 'bold 12px sans-serif', stroke: '#000', margin: new go.Margin(0, 4, 0, 0), text: "Tipo:" }),
-          $(go.TextBlock, { font: '12px sans-serif', stroke: '#000', width: 320, wrap: go.TextBlock.WrapFit }, new go.Binding('text', 'type'))
+        $(go.Panel, "Horizontal", { defaultAlignment: go.Spot.Left, margin: new go.Margin(12, 0, 0, 0) },
+          $(go.TextBlock, { font: 'bold 12px sans-serif', stroke: '#000', margin: new go.Margin(0, 8, 0, 10), text: "Tipo:" }),
+          $(go.TextBlock, { font: '14px sans-serif', stroke: '#000', width: 320, wrap: go.Wrap.Fit }, new go.Binding('text', 'type'))
         ),
 
-        $(go.Panel, "Horizontal", { defaultAlignment: go.Spot.Left, margin: new go.Margin(10, 0, 0, 0) },
-          $(go.TextBlock, { font: 'bold 12px sans-serif', stroke: '#000', margin: new go.Margin(0, 4, 0, 0), text: "Unidade:" }),
-          $(go.TextBlock, { font: '12px sans-serif', stroke: '#000', width: 220, wrap: go.TextBlock.WrapFit }, new go.Binding('text', 'unidade'))
+        $(go.Panel, "Horizontal", { defaultAlignment: go.Spot.Left, margin: new go.Margin(12, 0, 0, 0) },
+          $(go.TextBlock, { font: 'bold 12px sans-serif', stroke: '#000', margin: new go.Margin(0, 8, 0, 10), text: "Unidade:" }),
+          $(go.TextBlock, { font: '14px sans-serif', stroke: '#000', maxSize: new go.Size(210, NaN), wrap: go.Wrap.Fit }, new go.Binding('text', 'unidade'))
         ),
 
-        $(go.Panel, "Horizontal", { defaultAlignment: go.Spot.Left, margin: new go.Margin(10, 0, 0, 0) },
-          $(go.TextBlock, { font: 'bold 12px sans-serif', stroke: '#000', margin: new go.Margin(0, 4, 0, 0), text: "Instância:" }),
-          $(go.TextBlock, { font: '12px sans-serif', stroke: '#000', width: 300, wrap: go.TextBlock.WrapFit }, new go.Binding('text', 'instance'))
+        $(go.Panel, "Horizontal", { defaultAlignment: go.Spot.Left, margin: new go.Margin(12, 0, 15, 0) },
+          $(go.TextBlock, { font: 'bold 12px sans-serif', stroke: '#000', margin: new go.Margin(0, 8, 0, 10), text: "Instância:" }),
+          $(go.TextBlock, { font: '14px sans-serif', stroke: '#000', width: 300, wrap: go.Wrap.Fit }, new go.Binding('text', 'instance'))
         ),
 
-        $(go.Shape, "LineH", { stroke: "#FFF", strokeWidth: 2, width: 260, margin: new go.Margin(-10, 0, -10, 0) }),
-
-        $(go.Panel, "Vertical", { defaultAlignment: go.Spot.Left, margin: new go.Margin(4, 0, 0, 0) },
-          $(go.TextBlock, { font: 'bold 12px sans-serif', stroke: '#000', margin: new go.Margin(0, 0, 5, 0), text: "Descrição 📝:" }),
-          $(go.TextBlock, { font: '12px sans-serif', stroke: '#000', width: 260, wrap: go.TextBlock.WrapFit, margin: new go.Margin(0, 0, 5, 0), spacingAbove: 3 }, new go.Binding('text', 'descricao'))
+        // $(go.Shape, "LineH", { stroke: "#FFF", strokeWidth: 2, width: 300, margin: new go.Margin(0, 0, 0, 0) }),
+        $(go.Panel, "Auto",
+          $(go.Shape, "RoundedRectangle", { alignment: go.Spot.Center }, { fill: "#FFFFFF", width: 280, stroke: "transparent", parameter1: 5 }),
+          $(go.Panel, "Vertical", { alignment: go.Spot.Left, defaultAlignment: go.Spot.Left },
+            $(go.TextBlock, { font: 'bold 14px sans-serif', stroke: '#000', margin: new go.Margin(12, 8, 4, 8), text: "Descrição 📝:" }),
+            $(go.TextBlock, { font: '14px sans-serif', stroke: '#000', maxSize: new go.Size(260, NaN), wrap: go.Wrap.Fit, margin: new go.Margin(4, 8, 12, 8), spacingAbove: 3 }, new go.Binding('text', 'descricao'))
+          )
         )
       )
     );
@@ -115,5 +128,16 @@ export class GojsFlowchartComponent implements AfterViewInit {
     model.nodeParentKeyProperty = 'next';
     model.nodeDataArray = MARCOS_PROCESSO;
     myDiagram.model = model;
+  }
+
+  downloadAsPng() {
+    const div = this.captureDiv.nativeElement;
+
+    html2canvas(div).then(canvas => {
+      const link = document.createElement('a');
+      link.href = canvas.toDataURL('image/png');
+      link.download = 'marcos-processo.png';
+      link.click();
+    });
   }
 }
